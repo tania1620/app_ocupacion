e476dcb89b6c6a5f0ff095cee2fa86763ddc2249
tania1620 <taniacamachoarroyo@gmail.com>
2024-09-13 17:34:12 +0200

    Add files via upload

--- codigo_de_ejecucion.py
+++ codigo_de_ejecucion.py
@@ -129,7 +129,7 @@ def ejecutar_modelos(df):
     x = eliminar_duplicados(calidad_datos(df))
                      
     #3.CARGA PIPE DE EJECUCION
-    with gzip.open('tania1620/app_ocupacion/main/pipe_ejecucion.pickle', mode='rb') as file:
+    with gzip.open('/app_ocupacion/pipe_ejecucion.pickle', mode='rb') as file:
         pipe_ejecucion = pickle.load(file)
 
     #EJECUCION
